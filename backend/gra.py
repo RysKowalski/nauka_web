56b49f936d19d0cc0b4619c64eb20b7f9652ace4
RysKowalski <153722789+RysKowalski@users.noreply.github.com>
2024-12-10 09:54:53 +0100

    Update gra.py

--- backend/gra.py
+++ backend/gra.py
@@ -1,2 +1,21 @@
-def g():
-	...
+from typing import Self
+
+class Game:
+	def __init__(self: Self, elements: list[str]) -> None:
+		...
+
+class Instances:
+	def __init__(self: Self) -> None:
+		self.instances: dict[int, Game] = {}
+	
+	def new_instance(self: Self, id: int, elements: list[str]) -> None:
+		new_instance: Game = Game(elements)
+		self.instances[id] = new_instance
+
+
+if __name__ == '__main__':
+	instances: Instances = Instances()
+	
+	instances.new_instance(4, ['test1', 'test2'])
+	
+	print(instances.instances)
