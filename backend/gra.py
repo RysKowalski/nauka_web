3c5adc8f54ccae08b25601ca9ffe0137a758570b
RysKowalski <153722789+RysKowalski@users.noreply.github.com>
2024-12-10 10:42:01 +0100

    Update gra.py

--- backend/gra.py
+++ backend/gra.py
@@ -3,6 +3,9 @@ from typing import Self
 class Game:
 	def __init__(self: Self, elements: list[str]) -> None:
 		...
+	
+	def action(self: Self, endtime: int, odpowiedz: bool) -> dict:
+		return {}
 
 class Instances:
 	def __init__(self: Self) -> None:
@@ -12,10 +15,16 @@ class Instances:
 		new_instance: Game = Game(elements)
 		self.instances[id] = new_instance
 
+	def update_instance(self: Self, id: int, endtime: int, odpowiedz: bool) -> dict:
+		
+		new_data: dict = self.instances[id].action(endtime, odpowiedz)
+		return new_data
 
 if __name__ == '__main__':
 	instances: Instances = Instances()
 	
 	instances.new_instance(4, ['test1', 'test2'])
 	
-	print(instances.instances)
+	data: dict = instances.update_instance(4, 30000, True)
+
+	print(instances.instances, '\n\n\n\n', data)
