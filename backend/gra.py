040b848acca94472b4fd4bbdd9d474f270808f21
RysKowalski <153722789+RysKowalski@users.noreply.github.com>
2024-12-17 10:36:36 +0100

    Update gra.py

--- backend/gra.py
+++ backend/gra.py
@@ -17,18 +17,26 @@ class Game:
 			
 		self.points: int = 0
 		
-		
-		with 
-		self.names: list[str] = 
+		self.questions: list[str] = []
+		self.answers: list[str] = []
+
+		with open(os.path.join('data', 'nauka_questions.yaml')) as plik:
+			questions: dict = yaml.safe_load(plik)
+			for element in elements:
+				self.questions.append(questions[element]['names'])
+				self.answers.append(questions[element]['data'])
+
 		
 		print(f'{user_data = } \n')
 		print(f'{self.max_points = } \n')
 		print(f'{self.chances = } \n')
+		print(f'{self.questions = } \n')
+		print(f'{self.answers = } \n')
 		
 
 class Instances:
 	def __init__(self: Self) -> None:
-		self.instances: dict[int, Game] = {}
+		self.instances: dict[str, Game] = {}
 	
 	def new_instance(self: Self, name: str, elements: list[str]) -> None:
 		new_instance: Game = Game(elements, name)
