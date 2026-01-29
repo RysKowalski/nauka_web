7cfa2a0b90b82a8a6dc5a94a636cd7869c0b43af
RysKowalski <ryskowalski0@gmail.com>
2026-01-29 23:18:06 +0100

    added add_user and get_user_by_discordID functions and tests

--- nauka_web_api/database.py
+++ nauka_web_api/database.py
@@ -2,6 +2,15 @@ from contextlib import contextmanager
 import sqlite3
 from sqlite3 import Connection, Cursor
 from typing import Iterator
+from dataclasses import dataclass
+
+
+@dataclass
+class User:
+    discordId: int
+    username: str
+    visibleName: str
+    avatarLink: str
 
 
 class Database:
@@ -24,36 +33,92 @@ class Database:
             cursor: Cursor = conn.cursor()
 
             cursor.execute("""
-            CREATE TABLE IF NOT EXISTS users (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                discord_id TEXT UNIQUE NOT NULL,
-                username TEXT NOT NULL,
-                global_name TEXT NOT NULL,
-                avatar TEXT,
-                api_key TEXT UNIQUE NOT NULL
-            )
+                CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    discord_id INTEGER UNIQUE NOT NULL,
+                    username TEXT NOT NULL,
+                    visible_name TEXT NOT NULL,
+                    avatar TEXT NOT NULL,
+                    api_key TEXT UNIQUE NOT NULL
+                )
             """)
 
             cursor.execute("""
-            CREATE TABLE IF NOT EXISTS modules (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT NOT NULL,
-                user_id TEXT NOT NULL,
-                FOREIGN KEY (user_id) REFERENCES users(id),
-                UNIQUE (name, user_id)
-            )
+                CREATE TABLE IF NOT EXISTS modules (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL,
+                    user_id TEXT NOT NULL,
+                    FOREIGN KEY (user_id) REFERENCES users(id),
+                    UNIQUE (name, user_id)
+                )
             """)
 
             cursor.execute("""
-            CREATE TABLE IF NOT EXISTS entries (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                module_id INTEGER NOT NULL,
-                position INTEGER NOT NULL,
-                question TEXT NOT NULL,
-                answer TEXT NOT NULL,
-                FOREIGN KEY (module_id) REFERENCES modules(id),
-                UNIQUE (module_id, position)
-            )
+                CREATE TABLE IF NOT EXISTS entries (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    module_id INTEGER NOT NULL,
+                    position INTEGER NOT NULL,
+                    question TEXT NOT NULL,
+                    answer TEXT NOT NULL,
+                    FOREIGN KEY (module_id) REFERENCES modules(id),
+                    UNIQUE (module_id, position)
+                )
+            """)
+
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS chances (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    entry_id INTEGER NOT NULL,
+                    user_id INTEGER NOT NULL,
+                    chance REAL NOT NULL
+                )
             """)
 
             conn.commit()
+
+    def add_user(self, userData: User, apiKey: str):
+        with self._connect() as conn:
+            cursor: Cursor = conn.cursor()
+
+            cursor.execute(
+                """
+                INSERT INTO users
+                (discord_id, username, visible_name, avatar, api_key)
+                VALUES (?, ?, ?, ?, ?)
+            """,
+                (
+                    userData.discordId,
+                    userData.username,
+                    userData.visibleName,
+                    userData.avatarLink,
+                    apiKey,
+                ),
+            )
+
+            conn.commit()
+
+    def get_user_by_discordId(self, discordId) -> User:
+        USERNAME_INDEX: int = 0
+        VISIBLE_NAME_INDEX: int = 1
+        AVATAR_INDEX: int = 2
+        with self._connect() as conn:
+            cursor: Cursor = conn.cursor()
+
+            cursor.execute(
+                """
+                SELECT username, visible_name, avatar
+                FROM users
+                WHERE discord_id = ?
+            """,
+                (discordId,),
+            )
+
+            output: tuple[str, str, str] = cursor.fetchone()
+
+        userData: User = User(
+            discordId,
+            output[USERNAME_INDEX],
+            output[VISIBLE_NAME_INDEX],
+            output[AVATAR_INDEX],
+        )
+        return userData
--- tests.py
+++ tests.py
@@ -1,19 +1,74 @@
-from sqlite3 import Connection
-from nauka_web_api.database import Database
+from sqlite3 import Connection, Cursor
+import sqlite3
+from nauka_web_api.database import Database, User
+import os
 
 
 class Utils:
     DBPATH: str = "test_db.sqlite"
 
+    def get_uninitialized_db(self) -> Database:
+        os.remove(self.DBPATH)
+        db: Database = Database(self.DBPATH)
+        return db
+
+    def get_db(self) -> Database:
+        os.remove(self.DBPATH)
+        db: Database = Database(self.DBPATH)
+        db.create_db()
+        return db
+
 
 utils: Utils = Utils()
 
 
-# def test_database_conn_creation() -> None:
-#     class TestDB(Database):
-#         def test(self):
-#             with self.connect() as conn:
-#                 return type(conn)
-#
-#     print(TestDB(utils.DBPATH).test())
+def test_database_file_creation() -> None:
+    db: Database = utils.get_uninitialized_db()
+    db.create_db()
+    assert os.path.isfile(utils.DBPATH)
 
+
+def test_database_add_user() -> None:
+    db: Database = utils.get_db()
+    userId: int = 1234
+    userData: User = User(
+        userId, "username", "User Name", "https://www.pngmart.com/image/159906"
+    )
+    apiKey: str = "epicSecretKey"
+
+    db.add_user(userData, apiKey)
+
+    conn: Connection = sqlite3.connect(utils.DBPATH)
+    cursor: Cursor = conn.cursor()
+    cursor.execute(
+        """
+        SELECT discord_id, username, visible_name, avatar, api_key
+        FROM users
+        WHERE discord_id = ?
+    """,
+        (userId,),
+    )
+
+    output = cursor.fetchone()
+
+    assert output == (
+        userId,
+        "username",
+        "User Name",
+        "https://www.pngmart.com/image/159906",
+        apiKey,
+    )
+
+
+def test_database_get_user() -> None:
+    db: Database = utils.get_db()
+    userId: int = 1234
+    userData: User = User(
+        userId, "username", "User Name", "https://www.pngmart.com/image/159906"
+    )
+    apiKey: str = "apikey"
+    db.add_user(userData, apiKey)
+
+    output: User = db.get_user_by_discordId(userId)
+
+    assert output == userData
